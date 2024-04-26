972eaf973a93df795064760797faa8e87efa9bda
arturop <67555134+arturopstem@users.noreply.github.com>
2024-04-26 10:43:07 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const config = {
+  entry: './src/main.js',
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin(),
+    new ESLintPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+
+  output: {
+    clean: true,
+  },
+
+  devServer: { watchFiles: './src/**/*' },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'inline-source-map';
+  }
+  if (argv.mode === 'production') {
+    config.optimization = {
+      minimizer: [new CssMinimizerPlugin(), '...'],
+    };
+  }
+  return config;
+};
