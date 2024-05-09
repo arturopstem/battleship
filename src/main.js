63e6a19a370b79adfad3365d3d484a298e15db8b
arturop <67555134+arturopstem@users.noreply.github.com>
2024-05-09 17:53:53 -0500

    feat: activate components and initialize game

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
+import * as dom from './dom';
+import * as coords from './game/coordinates';
+import { isPieceAvailable, newSetOfShipPieces } from './game/pieces';
+import Player from './game/player';
 import './index.css';
+
+dom.registerComponents();
+
+dom.init();
+
+const computerPieces = newSetOfShipPieces();
+const computer = new Player('computer');
+
+Object.keys(computerPieces).forEach((piece) => {
+  while (isPieceAvailable(piece, computerPieces)) {
+    const allShipsCoords = coords.allShipsCoords(computer.gameboard.ships);
+
+    let shipCoords = coords.randomShipCoords(piece);
+    while (coords.hasOverlap(shipCoords, allShipsCoords)) {
+      shipCoords = coords.randomShipCoords(piece);
+    }
+
+    computer.gameboard.placeShip(...shipCoords);
+    computerPieces[piece] -= 1;
+  }
+});
+
+const realPlayerPieces = newSetOfShipPieces();
+const realPlayer = new Player('real');
+
+dom.activateForm(realPlayer, realPlayerPieces);
+dom.activateTrackingBoard(realPlayer, computer);
